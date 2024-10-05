11c202eceb3995dc94c898cd7eed2bfe90c126c4
Dinesh Ojha <dineshojha1013@gmail.com>
2024-10-05 12:17:10 +0545

    Add user role and pass data from token to session.

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,13 @@
 import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
 
 export default async function SettingsPage() {
+	const session = await auth();
+
 	return (
 		<>
+			<div>Settings: {JSON.stringify(session)}</div>
 			<form
 				action={async () => {
 					"use server";
--- auth.ts
+++ auth.ts
@@ -2,14 +2,37 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 
 const prisma = new PrismaClient();
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+	//https://next-auth.js.org/configuration/callbacks
+
+	callbacks: {
+		// get access to the user data - first get the id from token, then
+		// get the user from the database, then add the role to the token
+		jwt: async ({ token }) => {
+			if (token.sub) {
+				const user = await getUserById(token.sub);
+				if (user) {
+					token.role = user.role;
+				}
+			}
+			return token;
+		},
+		session: async ({ session, token }) => {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+				session.user.role = token.role;
+			}
+			return session;
+		},
+	},
 	adapter: PrismaAdapter(prisma),
 	session: { strategy: "jwt" },
 	...authConfig,
 });
 
-// with prisma we cannot use the database session as it doesnt work in edge?? hennce didnt use it
+// with prisma we cannot use the database session as it doesnt work in edge?? hence didnt use it
 // we use jwt session instead as in the doc
